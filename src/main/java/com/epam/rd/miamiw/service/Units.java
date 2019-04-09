67bd0ef11b4299079f29ec4668e2362c464251b7
Vladyslav Sereda <vladyslav_sereda1@epam.com>
2019-04-09 09:59:15 +0300

    Add factory method

--- src/main/java/com/epam/rd/miamiw/service/Units.java
+++ src/main/java/com/epam/rd/miamiw/service/Units.java
@@ -1,5 +1,15 @@
 package com.epam.rd.miamiw.service;
 
 public enum Units {
-    METRIC, IMPERIAL
+    METRIC, IMPERIAL;
+
+    public static Units fromLiteral(String literal) {
+        switch (literal.toLowerCase()) {
+            case "i":
+                return IMPERIAL;
+            case "m":
+            default:
+                return METRIC;
+        }
+    }
 }
